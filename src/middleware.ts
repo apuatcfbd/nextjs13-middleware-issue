522232989f0d7b0b5a4a55d5e6b8a4b2b81e134f
Mohammad A. Apu <74357797+apuatcfbd@users.noreply.github.com>
2023-11-04 21:42:07 +0600

    Checked the issue in the latest canary

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
 // This function can be marked `async` if using `await` inside
+import {NextRequest, NextResponse} from "next/server";
+
 export function middleware(request: NextRequest) {
-  console.log(Math.random() + ' Middleware ran')
+  console.log(Math.random() + ' Middleware ran');
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
